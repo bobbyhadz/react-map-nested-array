f8c97a39658d7d4e21f861277d1cd6216280d04e
bobbyhadz <opmftw@gmail.com>
2024-04-06 16:36:48 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+export default function App() {
+  const people = [
+    {id: 1, name: 'Alice', pets: ['dog', 'cat']},
+    {id: 2, name: 'Bob', pets: ['turtle', 'rabbit']},
+    {id: 3, name: 'Carl', pets: ['hamster', 'parrot']},
+  ];
+
+  return (
+    <div>
+      {people.map((person, index) => {
+        return (
+          <div key={index}>
+            <h2>Name: {person.name}</h2>
+
+            {person.pets.map((pet, index) => {
+              return (
+                <div key={index}>
+                  <h2>Pet: {pet}</h2>
+                </div>
+              );
+            })}
+
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
